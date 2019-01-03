1ead438c60cfea80cb40a7e1c12fbf1404a36f34
jy-sunshine <842285950@qq.com>
2019-01-03 22:40:30 +0800

    make main.c change

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+typedef struct{
+    int Id;
+    const char *name;
+    const char *pwd;
+}usr_data;
 
 int main(int argc, char *argv[])
 {
     printf("helloworld\n");
+    usr_data *usr = NULL;
     return 0;
 }
