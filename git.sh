7499f01169c3db05eff879ca2d6e859b4d2ff571
Txhua <842285950@qq.com>
2019-06-19 15:07:47 +0800

    modify

--- git.sh
+++ git.sh
@@ -1,6 +1,44 @@
 # git shell
 
+set -e
 set -u
+
+# 定位到当前目录
+base_path=$(
+	cd "$(dirname "$0")"
+	pwd
+)
+
+cd "$base_path"
+message=""
+origin=""
+while getopts 'm:r:' OPT; do
+	case $OPT in
+	m)
+		message="$OPTARG"
+		;;
+	r)
+		origin="$OPTARG"
+		;;
+	?)
+		echo "Usage: $(basename $0) [OPTIONS] [database]"
+		echo " -m message."
+		echo " -r origin."
+		exit
+		;;
+	esac
+done
+shift $(($OPTIND - 1))
+
+if [ ${message} == "" ];then
+	echo "message is null"
+	exit;
+fi
+
+if [ ${origin} == "" ];then
+	echo "origin is null";
+	exit;
+fi
 
 # 拉取最新的文件
 git pull 
@@ -9,8 +47,8 @@ git pull
 git add .
 
 # 提交文件
-git commit -m "modify"
+git commit -m ${message}
 
 # 提交文件到远程仓库
-git push -u origin master
+git push -u ${origin} master
 
